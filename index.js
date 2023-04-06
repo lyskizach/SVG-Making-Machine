0da9cda962891207f194230625afafd0fa53df7d
lyskz <lyski.zach@icloud.com>
2023-04-06 13:30:40 -0700

    added .then to generate the svg file of corresponding shape. need to write shape functions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const circleLogo = requie('./lib/circle');
+const circleLogo = require('./lib/circle');
 const triangleLogo = require('./lib/triangle');
 const squareLogo = require('./lib//square');
 
@@ -25,3 +25,25 @@ inquirer
             message: 'What color would you like the logo to be? (Enter color keyword or hexadecimal number)'
         }
     ])
+    
+    .then((data) => {
+        const shape = data.shape;
+        let svgShape;
+        switch (shape) {
+            case 'circle':
+                svgShape = new circleLogo(data);
+                fs.writeFile('logo.svg', svgShape.render(data), err => {
+                    err ? console.log(err) : console.log('Successfully generated!')
+                });
+            case 'triangle':
+                svgShape = new triangleLogo(data);
+                fs.writeFile('logo.svg', svgShape.render(data), err => {
+                    err ? console.log(err) : console.log('Successfully generated!')
+                });
+            case 'square':
+                svgShape = new squareLogo(data);
+                fs.writeFile('logo.svg', svgShape.render(data), err => {
+                    err ? console.log(err) : console.log('Successfully generated!')
+                });
+        }
+    })
