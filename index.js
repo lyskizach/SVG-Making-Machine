3f8a10907a199f61a64491e5f6765ec72479638d
lyskz <lyski.zach@icloud.com>
2023-04-06 13:01:06 -0700

    added inquirer prompt questions

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'txt',
+            message: 'Please enter the characters within your logo (Up to 3 total)'
+        }, {
+            type: 'input',
+            name: 'txtColor',
+            message: 'What color of text would you like to use? (Enter color keyword or hexadecimal number)'
+        }, {
+            type: 'list',
+            name: 'shape',
+            message: 'What shape would you like your logo to be?',
+            choices: ['circle', 'triangle', 'square']
+        }, {
+            type: 'input',
+            name: 'shaepColor',
+            message: 'What color would you like the logo to be? (Enter color keyword or hexadecimal number)'
+        }
+    ])
